f3be699df89ff0fd2e4cbae84bcf967c020a9e41
ls1142 <ls1142@exeter.ac.uk>
2025-01-31 14:02:28 +0000

    Update Louis.py

--- Louis.py
+++ Louis.py
@@ -1,14 +1,36 @@
-import random
+import random, time
+
+
+start = time.time()
+
 
 n=10
 elevator = [0,-1] #0-n
 floors = {}
+p = 0
+capacity = 1
+
+
+
+
 for i in range(0, n):
     floors[str(i)] = [random.randint(0, n-1) for i in range(0, random.randint(0, 3))]
+    p+=len(floors[str(i)])
 
 
+def checkIfEmpty(floors):
+    flag = True
+    for i in range(0,n):
+        if floors[str(i)] != []:
+            flag = False
+
+    return flag
+
+
+
+running = True
 flag = 1
-while True:
+while running:
     print("\033c")
     print(floors)
     print("You are at floor ", elevator[0])
@@ -31,4 +53,11 @@ while True:
         elevator[0] -= 1
         if elevator[0] == 0:
             flag = 1
-    input("Press Enter to continue...")
+    
+    if checkIfEmpty(floors):
+        running = False#
+
+
+end = time.time()
+print(f"runtime is: {end-start:.6f}s")
+print(f"people: {p}")
